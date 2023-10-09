7b1bdbdda567d3d223530d950164abfe53a13e7c
moses <mosesjul1425@gmail.com>
2023-10-09 19:41:33 +0700

    feat: add colour theme to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3FEF54',
+        secondary: '#9AECCE',
+        tertiary: '#5CEF79',
+        quarternary: '#78EA9C',
+      },
+    },
   },
   plugins: [],
 };
