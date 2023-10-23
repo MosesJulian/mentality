b6c25a3a8751b94b1576b2176b7a43929199d0b0
JosephJoshua <jj.anggita@gmail.com>
2023-10-23 11:41:24 +0700

    deploy: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   integrations: [solidJs(), tailwind()],
+  adapter: vercel()
 });
